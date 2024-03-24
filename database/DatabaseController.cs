a1084e936b944e3a41022f6082bfc5b92193744f
Caleb G <mc1174471@gmail.com>
2024-03-24 16:09:22 -0500

    fixed create user

--- database/DatabaseController.cs
+++ database/DatabaseController.cs
@@ -27,34 +27,23 @@ public class DatabaseController
 
     public string createUser(string username, string password)
     {
-        (string, bool?, bool?) userCredValidation = UserPassValid(username, password);
-        if (userCredValidation.Item2 && userCredValidation.Item3)
+        (string, bool) userCredValidation = UserPassValid(username, password);
+        if (userCredValidation.Item2)
             AddUser(new User());    //TODO: Finish User Field
         return userCredValidation.Item1;
     }
 
-    private (string, bool?, bool?) UserPassValid(string username, string password)
+    private (string, bool) UserPassValid(string username, string password)
     {
         if (string.IsNullOrWhiteSpace(username))
-            return ("Username field is blank", false, null);
+            return ("Username field is blank", false);
         if (string.IsNullOrWhiteSpace(password))
-            return ("Password field is blank", null, false);
-
-        bool freeUserName = true;
-        foreach (User user in GetAllUsers())
-        {
-            if (username.Equals(user.username))
-            {
-                freeUserName = false;
-                break;
-            }
-        }
-
-        if (!freeUserName)
-            return ("Username already exists", false, null);
+            return ("Password field is blank", false);
+        if (GetUserFromUsername(username) != null)
+            return ("Username already exists", false);
         if (password.Length < 8 || !password.Any(char.IsUpper) || !password.Any(char.IsLower) || !password.Any(char.IsNumber))
-            return ("Pasword must be at least 8 characters long with at least one uppercase letter, lowercase letter, and number", true, false);
-        return ("Username and Password are Valid", true, true);
+            return ("Password must be at least 8 characters long with at least one uppercase letter, lowercase letter, and number", false);
+        return ("Username and Password are Valid", true);
     }
 
     public IEnumerable<User> GetAllUsers()
