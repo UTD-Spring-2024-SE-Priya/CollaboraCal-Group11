d84779ce8c3e39d94ddcf6cb53de88259cbb4176
Caleb G <mc1174471@gmail.com>
2024-03-24 16:46:55 -0500

    create user now adds a user to the database

---
+++ account/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+
+public class AccountController
+{
+    DatabaseController dbController;
+
+    public AccountController(DatabaseController databaseController) 
+    { 
+        dbController = databaseController; 
+    }
+    
+    public string createUser(string username, string password)
+    {
+        (string, bool) userCredValidation = UserPassValid(username, password);
+        if (userCredValidation.Item2)
+        {
+            User user = new()
+            {
+                Username = username,
+                PasswordHashData = new SecureHash<SHA256>(password)
+            };
+            dbController.AddUser(user);
+        }
+        return userCredValidation.Item1;
+    }
+
+    private (string, bool) UserPassValid(string username, string password)
+    {
+        if (string.IsNullOrWhiteSpace(username))
+            return ("Username field is blank", false);
+        if (string.IsNullOrWhiteSpace(password))
+            return ("Password field is blank", false);
+        if (dbController.GetUserFromUsername(username) != null)
+            return ("Username already exists", false);
+        if (password.Length < 8 || !password.Any(char.IsUpper) || !password.Any(char.IsLower) || !password.Any(char.IsNumber))
+            return ("Password must be at least 8 characters long with at least one uppercase letter, lowercase letter, and number", false);
+        return ("Username and Password are Valid", true);
+    }
+}
--- database/DatabaseController.cs
+++ database/DatabaseController.cs
@@ -25,27 +25,6 @@ public class DatabaseController
         dbContext.SaveChanges();
     }
 
-    public string createUser(string username, string password)
-    {
-        (string, bool) userCredValidation = UserPassValid(username, password);
-        if (userCredValidation.Item2)
-            AddUser(new User());    //TODO: Finish User Field
-        return userCredValidation.Item1;
-    }
-
-    private (string, bool) UserPassValid(string username, string password)
-    {
-        if (string.IsNullOrWhiteSpace(username))
-            return ("Username field is blank", false);
-        if (string.IsNullOrWhiteSpace(password))
-            return ("Password field is blank", false);
-        if (GetUserFromUsername(username) != null)
-            return ("Username already exists", false);
-        if (password.Length < 8 || !password.Any(char.IsUpper) || !password.Any(char.IsLower) || !password.Any(char.IsNumber))
-            return ("Password must be at least 8 characters long with at least one uppercase letter, lowercase letter, and number", false);
-        return ("Username and Password are Valid", true);
-    }
-
     public IEnumerable<User> GetAllUsers()
     {
         return dbContext.Users;
