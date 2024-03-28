96ce93784a4136ebaf2bef65b51a7a4b3c89d658
HaoleNetwork <mc1174471@gmail.com>
2024-03-27 19:52:54 -0500

    this will validate whether the email address entered is an actual email address or not

--- account/AccountController.cs
+++ account/AccountController.cs
@@ -29,6 +29,14 @@ public class AccountController
             return ("Password field is blank", false);
         if (Application.Database.GetUserFromEmail(email) != null)
             return ("Email already exists", false);
+
+        //  This will test if the entered email is an actual email or not
+        bool validEMail = true;
+        try { MailAddress address = new MailAddress(email); }
+        catch { validEMail = false; }
+
+        if (!validEMail)
+            return ("Couldn't verify email address", false);
         if (password.Length < 8 || !password.Any(char.IsUpper) || !password.Any(char.IsLower) || !password.Any(char.IsNumber))
             return ("Password must be at least 8 characters long with at least one uppercase letter, lowercase letter, and number", false);
         return ("Email and Password are Valid", true);
