791b2d63fe478d27e5f97a94d96057c2300b4c0a
Caleb G <mc1174471@gmail.com>
2024-03-29 18:55:27 -0500

    fixed solution directory

---
+++ Phase4TestCases/UnitTest1.cs
@@ -0,0 +1,14 @@
+// need to add using for namespace for test cases test
+
+namespace Phase4TestCases
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestEMailPassValid()
+        {
+            //Assert.AreEqual(("Email field is blank", false), );
+        }
+    }
+}
