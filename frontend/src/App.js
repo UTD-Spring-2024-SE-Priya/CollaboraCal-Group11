a8b76f6eef3d4283fda0a7de0a6d5f9cdc4a17cc
anwitagudapuri <143034795+anwitagudapuri@users.noreply.github.com>
2024-04-16 13:18:47 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginSignUp from './Components/LoginSignUp/LoginSignUp';
 import Main from './Components/Main/Main';
+import ForgotPassword from './Components/ForgotPassword/ForgotPassword';
+import CreateEvent from './Components/CreateEvent/CreateEvent';
+import CreateCalendar from './Components/CreateCalendar/CreateCalendar';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Routes>
           <Route path="/" element={<LoginSignUp />} />
           <Route path="/home" element={<Main />} />
+          <Route path="/reset-password" element={<ForgotPassword />} />
+          <Route path="/create-event" element={<CreateEvent />} />
+          <Route path="/create-calendar" element={<CreateCalendar />} />
         </Routes>
       </div>
     </Router>
